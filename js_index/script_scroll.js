6c27a38d238e15843d2143036022038630fe5122
borbapugliese <borbapugliese@hotmail.com>
2022-09-29 16:08:35 -0300

    update

--- js_index/script_scroll.js
+++ js_index/script_scroll.js
@@ -5,3 +5,9 @@ scrollDiv.addEventListener("onclick",scrollToTop())
 function scrollToTop(){
     window.scrollTo(0, 0);
 }
+
+if(screen.width < 1188){
+    document.getElementById('netuno_href').setAttribute('href', "#");
+} else{ 
+    document.getElementById('netuno_href').setAttribute('href', "sobre_netuno.html");
+}
