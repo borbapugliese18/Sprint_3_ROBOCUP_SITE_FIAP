f3b59d10bb86379f1383a6764ec115e359709221
borbapugliese <borbapugliese@hotmail.com>
2022-09-29 16:15:51 -0300

    update nav mobile

--- js_index/script_scroll.js
+++ js_index/script_scroll.js
@@ -8,6 +8,9 @@ function scrollToTop(){
 
 if(screen.width < 1188){
     document.getElementById('netuno_href').setAttribute('href', "#");
+    document.getElementById('quem_somos_href').setAttribute('href', "#");
+    
 } else{ 
     document.getElementById('netuno_href').setAttribute('href', "sobre_netuno.html");
+    document.getElementById('quem_somos_href').setAttribute('href', "quem_somos.html");
 }
